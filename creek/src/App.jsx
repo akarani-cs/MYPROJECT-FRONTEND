415789998529270a5f688a776342a9d24dc64199
AlvinKarani <alvinmudembei@gmail.com>
2025-08-26 19:41:44 +0300

    correction on react template

--- creek/src/App.jsx
+++ creek/src/App.jsx
@@ -1,24 +1,18 @@
 import { useState } from 'react'
-import Header from './components/Header'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-
     <>
 
-     <Header />
-    
-      <div className="flex items-center justify-center min-h-screen bg-gray-900">
-        <h1 className="text-4xl font-bold text-white">
-          🎬 Creek -Frontend
-        </h1>
-        
-      </div>
+    <p>Home</p> 
+      
     </>
-    
   )
 }
 
-export default App;
+export default App
