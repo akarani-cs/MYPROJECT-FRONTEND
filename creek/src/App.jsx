9f1d56c4dfea940e5df0124a9747de70414cb96f
akarani-cs <alvinmudembei@gmail.com>
2025-08-29 09:45:14 +0300

    added tailwind and configured correctly

--- creek/src/App.jsx
+++ creek/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
 
-    <p>Home</p> 
-      
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello  🚀</h1>
+    </div>
+
+
     </>
   )
 }
 
-export default App
+
